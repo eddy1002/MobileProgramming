8ba03b42e1d5ba4763151a42664781cfbbaeeb12
eddy1002 <eddy1001@naver.com>
2016-09-08 10:59:10 +0900

    소트와 서치 코드

--- src/Searching.java
+++ src/Searching.java
@@ -7,14 +7,14 @@ public class Searching {
 		if (args[center] == searchnum)
 			System.out.println(searchnum + "은 배열에 존재합니다.");
 		
-		else if (args[center] > searchnum){
+		else if (args[center] > searchnum){ //찾는 수가 중심값보다 작을 때
 			if (center > end)
 				System.out.println(searchnum + "은 배열에 존재하지 않습니다.");
 			else
 				search(args, start, center - 1, searchnum);
 		}
 		
-		else if (args[center] < searchnum){
+		else if (args[center] < searchnum){ //찾는 수가 중심값보다 클 떄
 			if (center < start)
 				System.out.println(searchnum + "은 배열에 존재하지 않습니다.");
 			else
